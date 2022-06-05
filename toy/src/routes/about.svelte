c0cc1f47c4670329cd5a64f0e57895c2db34dbd3
sinsky <wesky93@gmail.com>
2022-06-05 21:58:37 +0900

    환경 설정
    라우팅 테스트
    typescript 설정
    tailwindCSS 설정

---
+++ toy/src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>Abount</title>
+</svelte:head>
+
+<h1>this is about page</h1>
+
+<a href="/">home</a>
---
+++ toy/src/routes/index.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+  <title>Welcome</title>
+</svelte:head>
+
+
+
+<h1 class="hello">
+    Hello world!!
+</h1>
+
+<a href="/about">about</a>
+
+<style lang="postcss">
+.hello{
+    @apply text-3xl text-blue-500 hover:text-blue-900
+}
+</style>
